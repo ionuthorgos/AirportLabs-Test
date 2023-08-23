b85401c29308344f2a24b24d990f91252f3f8f89
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-23 15:29:09 +0300

    fix test

--- cypress/e2e/storeMagazin.spec.cy.js
+++ cypress/e2e/storeMagazin.spec.cy.js
@@ -4,11 +4,11 @@ describe('template spec', () => {
     // cy.get('button[class*="js-accept"]').click();
     
     // Do a search after an item
-    // cy.get('[name="query"]').click().type("Televizor");
-    // cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
+    cy.get('[name="query"]').click().type("Televizor");
+    cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
 
     // Add to cart the first element TV
-    // cy.get('button[type="submit"]:eq(1)').click();
+     cy.get('button[type="submit"]:eq(1)').click();
     
     // cy.get('[class="modal-header"]').should('be.visible');
     // cy.get('[aria-label="Inchide"]').click();
