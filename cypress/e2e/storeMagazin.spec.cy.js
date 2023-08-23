1ad63b1c8ab7bbf9c32b8c7f411193ebb63403eb
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-23 15:08:05 +0300

    decomment code lines

--- cypress/e2e/storeMagazin.spec.cy.js
+++ cypress/e2e/storeMagazin.spec.cy.js
@@ -1,21 +1,17 @@
 describe('template spec', () => {
   it('Add item and accessory', () => {
     cy.visit('https://www.emag.ro/');
-    // cy.get('button[class*="js-accept"]').click();
+    cy.get('button[class*="js-accept"]').click();
     
-    // // Do a search after an item
-    // cy.get('[name="query"]').click().type("Televizor");
-    // cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
+    // Do a search after an item
+    cy.get('[name="query"]').click().type("Televizor");
+    cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
 
-    // // Add to cart the first element TV
-    // cy.get('button[type="submit"]:eq(1)').click();
+    // Add to cart the first element TV
+    cy.get('button[type="submit"]:eq(1)').click();
     
-    // Issue: The following lines are commented, so no assertions or actions are performed.
     // cy.get('[class="modal-header"]').should('be.visible');
     // cy.get('[aria-label="Inchide"]').click();
-
-    // Add accessory based on manufacturer and with a minimum of 3 stars
-    // You need to implement this part of the test scenario
   });
   
 });
