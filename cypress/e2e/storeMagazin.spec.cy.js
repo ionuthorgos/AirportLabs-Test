27f3a8e5355bc3931279b85fa3be2f06d39afaa8
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-23 15:13:29 +0300

    comment

--- cypress/e2e/storeMagazin.spec.cy.js
+++ cypress/e2e/storeMagazin.spec.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('Add item and accessory', () => {
     cy.visit('https://www.emag.ro/');
-    cy.get('button[class*="js-accept"]').click();
+    // cy.get('button[class*="js-accept"]').click();
     
     // Do a search after an item
     // cy.get('[name="query"]').click().type("Televizor");
