be202f2fcab2ae6363d4e22a7e2096c6852c8843
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-23 16:19:39 +0300

    comment second problem

--- cypress/e2e/storeMagazin.spec.cy.js
+++ cypress/e2e/storeMagazin.spec.cy.js
@@ -3,15 +3,15 @@ describe('template spec', () => {
     cy.visit('https://www.emag.ro/');
     // cy.get('button[class*="js-accept"]').click();
     
-    // Do a search after an item
-    cy.get('[name="query"]').click().type("Televizor");
-    cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
+    // // Do a search after an item
+    // cy.get('[name="query"]').click().type("Televizor");
+    // cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
 
-    // Add to cart the first element TV
-     cy.get('button[type="submit"]:eq(1)').click();
+    // // Add to cart the first element TV
+    //  cy.get('button[type="submit"]:eq(1)').click();
     
-    // cy.get('[class="modal-header"]').should('be.visible');
-    // cy.get('[aria-label="Inchide"]').click();
+    // // cy.get('[class="modal-header"]').should('be.visible');
+    // // cy.get('[aria-label="Inchide"]').click();
   });
   
 });
