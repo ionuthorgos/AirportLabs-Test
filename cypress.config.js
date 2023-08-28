843c3a543504eea5cf7f58ebacdaa3912a9ea1ff
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-28 11:33:59 +0300

    update config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/spec.cy.js",
-    browser: 'chrome',
+    browser: 'firefox',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
