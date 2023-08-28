45f5cd0bb7add0424cf5295cbb52e8443db50237
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-28 14:12:24 +0300

    update tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/e2e/**",
-    browser: 'firefox',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -2,14 +2,17 @@ describe('template spec', () => {
   it('Add item and accessory', () => {
     cy.visit('https://www.emag.ro/');
     cy.get('button[class*="js-accept"]').click();
-    
+    cy.get('[data-section="loading"]').should('not.be.visible');
+    cy.get('[class*="js-dismiss-login-notice-btn"]').click();
+
     // Do a search after an item
-    cy.get('[name="query"]').click().type("Televizor");
+    cy.get('[name="query"]').type("Televizor");
+    cy.get('[data-section="loading"]').should('not.be.visible');
     cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
-    cy.wait(3000)
+    cy.get('[data-section="loading"]').should('not.be.visible');
+    cy.url().should('eq', 'https://www.emag.ro/search/Televizor?ref=effective_search');
     cy.get('[type="submit"][data-offer-id="102545610"]').should('be.visible');
-    cy.get('[class*="js-accept"]').click();
-    cy.get('div[class="searchbox-wrapper searchbox-dropdown-open"]').should('not.be.visible');
+    // cy.get('div[class="searchbox-wrapper searchbox-dropdown-open"]').should('not.be.visible');
 
     // Add to cart the first element TV
     cy.get('[type="submit"][data-offer-id="102545610"]').click();
