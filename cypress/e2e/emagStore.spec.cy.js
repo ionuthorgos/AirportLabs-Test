8fdf2232be0d04ae8ee3e7faf0d4e118fe94fc41
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-28 12:09:17 +0300

    add selector

--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -8,7 +8,7 @@ describe('template spec', () => {
     cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
     cy.wait(3000)
     // Add to cart the first element TV
-     cy.get('button[data-offer-id="102545610"]:eq(1)').click();
+     cy.get('[type="submit"][data-offer-id="102545610"]').click();
     
     // // cy.get('[class="modal-header"]').should('be.visible');
     // // cy.get('[aria-label="Inchide"]').click();
