293b174cc6d377959e8270670768fa3067d2ee09
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-28 11:59:02 +0300

    repair emagStore

--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -8,7 +8,7 @@ describe('template spec', () => {
     cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
 
     // Add to cart the first element TV
-     cy.get('button[type="submit"]:eq(1)').click();
+     cy.get('button[data-offer-id="102545610"]:eq(1)').click();
     
     // // cy.get('[class="modal-header"]').should('be.visible');
     // // cy.get('[aria-label="Inchide"]').click();
