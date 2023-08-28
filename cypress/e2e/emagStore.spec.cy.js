1988bf42af59063dd1e546ef2cd80625f8629544
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-28 14:37:03 +0300

    update tests

--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -2,17 +2,15 @@ describe('template spec', () => {
   it('Add item and accessory', () => {
     cy.visit('https://www.emag.ro/');
     cy.get('button[class*="js-accept"]').click();
-    cy.get('[data-section="loading"]').should('be.visible');
+    // cy.get('[data-section="loading"]').should('be.visible');
     cy.get('[class*="js-dismiss-login-notice-btn"]').click();
 
     // Do a search after an item
     cy.get('[name="query"]').type("Televizor");
     cy.get('[data-section="loading"]').should('be.visible');
     cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
-    cy.get('[data-section="loading"]').should('be.visible');
     cy.url().should('eq', 'https://www.emag.ro/search/Televizor?ref=effective_search');
     cy.get('[type="submit"][data-offer-id="102545610"]').should('be.visible');
-    // cy.get('div[class="searchbox-wrapper searchbox-dropdown-open"]').should('not.be.visible');
 
     // Add to cart the first element TV
     cy.get('[type="submit"][data-offer-id="102545610"]').click();
