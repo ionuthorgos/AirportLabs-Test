227a1a34946d746973af730655cda821ec4e9bfe
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-28 11:49:19 +0300

    add e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "**/.js",
+    specPattern: "**/e2e/**",
     browser: 'firefox',
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -1,14 +1,14 @@
 describe('template spec', () => {
   it('Add item and accessory', () => {
     cy.visit('https://www.emag.ro/');
-    // cy.get('button[class*="js-accept"]').click();
+    cy.get('button[class*="js-accept"]').click();
     
-    // // Do a search after an item
-    // cy.get('[name="query"]').click().type("Televizor");
-    // cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
+    // Do a search after an item
+    cy.get('[name="query"]').click().type("Televizor");
+    cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
 
-    // // Add to cart the first element TV
-    //  cy.get('button[type="submit"]:eq(1)').click();
+    // Add to cart the first element TV
+     cy.get('button[type="submit"]:eq(1)').click();
     
     // // cy.get('[class="modal-header"]').should('be.visible');
     // // cy.get('[aria-label="Inchide"]').click();
