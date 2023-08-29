166e253df232dc8af56e639a12b288ec379c5ce0
Ionut Horgos <horgosdumitru@gmail.com>
2023-08-29 11:46:47 +0300

    update github actions

--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -12,9 +12,9 @@ describe('template spec', () => {
     cy.url().should('eq', 'https://www.emag.ro/search/Televizor?ref=effective_search');
     // cy.get('[type="submit"][data-offer-id="102545610"]').should('be.visible');
 
-    cy.wait(5000)
+    // cy.wait(5000)
     // Add to cart the first element TV
-    cy.get('[type="submit"][data-offer-id="102545610"]').click();
+    // cy.get('[type="submit"][data-offer-id="102545610"]').click();
     
     // cy.get('[class="modal-header"]').should('be.visible');
   });
