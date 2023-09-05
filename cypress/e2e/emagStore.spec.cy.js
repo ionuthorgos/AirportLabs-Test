823f366204a5dfe523fcd2ba5ef9c71159892338
Ionut Horgos <horgosdumitru@gmail.com>
2023-09-05 16:53:10 +0300

    add product

--- cypress/e2e/emagStore.spec.cy.js
+++ cypress/e2e/emagStore.spec.cy.js
@@ -11,11 +11,18 @@ describe('template spec', () => {
     cy.get('[data-section="loading"]').should('be.visible');
     cy.get('button[class="btn btn-default searchbox-submit-button"]').click();
     cy.url().should('eq', 'https://www.emag.ro/search/Televizor?ref=effective_search');
-    GenericFunctions.validateElementContainsText('[class="title-phrasing title-phrasing-xl"]','text', '"Televizor"');
-    // cy.wait(5000)
+    cy.get('[class="card-v2-content"] button:eq(1)').click();
+
+    
+    // cy.get('[data-zone="thumbnail"]:eq(1)').click();
+    // cy.get('[class="main-product-form"] button:eq(0)').click();
+
+    // cy.contains('Adauga in Cos').click()
+    // GenericFunctions.validateElementContainsText('[class="title-phrasing title-phrasing-xl"]','text', '"Televizor"');
+   cy.wait(5000)
     // Add to cart the first element TV
-    cy.get('[type="submit"][data-offer-id="102545610"]').click();
-    GenericFunctions.validateElementContainsText('h4[class="mrg-sep-none"]','text', 'Produsul a fost adaugat in cos');
+    // cy.get('[type="submit"][data-offer-id="102545610"]').click();
+    // GenericFunctions.validateElementContainsText('h4[class="mrg-sep-none"]','text', 'Produsul a fost adaugat in cos');
 
     
   });
---
+++ cypress/support/emagStore.js
@@ -0,0 +1,9 @@
+class EmagStore {
+
+    
+    getCardResults(){
+
+    }
+
+}
+export default new EmagStore();
